ad5316897b0e21ff717a9f8c4bdb396b568acaa0
salman-q <salman.qureshi1470@gmail.com>
2023-01-06 19:10:34 +0530

    More UI updates.

---
+++ app/src/main/java/com/example/calculator/Calc.kt
@@ -0,0 +1,41 @@
+package com.example.calculator
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Calc(
+    state: CalcState,
+    buttonSpacing: Dp = 8.dp,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(buttonSpacing)
+        ) {
+            Text(
+                text = state.number1 + (state.operation ?: "") + state.number2,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                fontWeight = FontWeight.Light,
+                fontSize = 80.sp,
+                color = MaterialTheme.colors.onBackground,
+                maxLines = 2
+            )
+        }
+    }
+}
